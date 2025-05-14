4bbabe2a89de4376ce6d80af28a5abfa0bb906c3
Manuela <manu.marin.vasquez.21@gmail.com>
2025-05-14 13:14:53 +0200

    Primer commit

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+//import './topics/01-basic-types-ts';
+import './topics/02-object-interface';
+import './topics/03-functions';
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML="Hola Mundo";
+
